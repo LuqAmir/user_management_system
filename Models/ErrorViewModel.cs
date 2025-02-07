64e10fcae792792b6195a5b6af075e95454e17e1
Muhammad Luqman <luqman.amiruddin@gmail.com>
2025-02-07 09:07:54 +0800

    first update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace user_management_system.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
